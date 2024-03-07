778e97fce438312cb015eb613b9a616668661f2b
NattoMillin <zz112ggww@icloud.com>
2024-03-08 01:57:33 +0900

    Todoリスト完成！わかりやすかった。

--- django/helloworldproject/helloworldproject/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import helloworldfunction, HelloWorldClass
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("helloworld/", helloworldfunction),
-    path("helloworld2/", HelloWorldClass.as_view()),
-]
